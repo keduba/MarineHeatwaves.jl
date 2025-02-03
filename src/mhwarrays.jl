31ba05b4f84485d0e4b462a4e25d59cb133b3cb2
Kene Uba <jfk.uba@protonmail.ch>
2025-02-03 17:03:35 +0100

    named variables consistently

--- src/mhwarrays.jl
+++ src/mhwarrays.jl
@@ -1,3 +1,8 @@
+# TODO 
+# rewrite all the variables to have a consistent name for tracking across the file.
+#
+#-
+
 using Base.Iterators: flatten
 
 abstract type MarineHeatWave end
@@ -72,56 +77,20 @@ end
 
 function _subtemp(sst::Array, mhwix)
     xz = seamask(sst)
-    xyz = CartesianIndices(xz)[xz]
-    msst = sst[xyz, mhwix]
-    return msst, xyz
+    mask = CartesianIndices(xz)[xz]
+    msst = sst[mask, mhwix]'
+    return msst, mask
 end
 
 function subtemp(sst, sstdate, evdate)
-    etix, elyd = timeindices(sstdate, evdate)
-    evsst, mask = _subtemp(sst, etix)
-    return evsst, mask, elyd
+    mhwix, elyd = timeindices(sstdate, evdate)
+    emsst, mask = _subtemp(sst, mhwix)
+    return emsst, mask, elyd
 end
 
 function findices(av, dr)
     (av[i] for i in flatten(dr))
 end
-
-function matevents(msms::MarineHW, msst::MarineHeatWave, mexc::BitMatrix, mstartsxs, mendsxs)
-    mni, cmi, mxi, drt, cats, rons, rdcs, vri, sdt, edt, cls, rws = ntuple(_ -> Vector(undef, length(mstartsxs)), 12)
-    fullyears = unique(year.(msst.dates))
-    mask = msst.mask
-    x, y = size(msms.temp)
-    climout, threshout = ntuple(_ -> similar(msms.temp, x, y, 366), 2)
-    msms.exceed[mask, :] = mexc
-    for i in eachindex(mstartsxs)
-        evmeta = _eventmetrics(eachrow(msst.temp)[i], eachrow(msst.clima)[i], eachrow(msst.thresh)[i], msst.lyday, msst.anomfn, msst.argfn, msst.dates, mstartsxs[i], mendsxs[i])
-        cats[i] = evmeta[4]
-        msms.temp[mask[i], :] = evmeta[7]
-        msms.category[mask[i], :] = evmeta[8]
-        climout[mask[i], :] = eachrow(msst.clima)[i]
-        threshout[mask[i], :] = eachrow(msst.thresh)[i]
-        meanmets, evmets = meanmetrics(evmeta[1], evmeta[2], evmeta[3], fullyears)
-        rons[i], rdcs[i], sdt[i], edt[i] = evmeta[2], evmeta[3], evmeta[5], evmeta[6]
-        rws[i] = repeat([mask[i][1]], length(evmeta[2]))
-        cls[i] = repeat([mask[i][2]], length(evmeta[2]))
-        mni[i], cmi[i], mxi[i], drt[i], vri[i] = evmets
-        anmets = annualmetrics(evmeta[1], evmeta[2], evmeta[3], evmeta[5], evmeta[6], fullyears)
-        trmets = trends(anmets)
-        for m in keys(msms.annuals)
-            msms.annuals[m][mask[i], :] = anmets[m]
-            msms.means[m][mask[i]] = meanmets[m]
-            msms.trends[m][mask[i]] = trmets.trends[m]
-            msms.pvalues[m][mask[i]] = trmets.pvalues[m]
-            msms.pmetrics[m][mask[i]] = trmets.pmetrics[m]
-        end
-    end
-
-    edf = (MeanInt=reduce(vcat, mni), CumInt=reduce(vcat, cmi), MaxInt=reduce(vcat, mxi), Duration=reduce(vcat, drt), Category=reduce(vcat, cats), ROnset=reduce(vcat, rons), RDecline=reduce(vcat, rdcs), VarInt=reduce(vcat, vri), StartDate=reduce(vcat, sdt), EndDate=reduce(vcat, edt), Xind=reduce(vcat, rws), Yind=reduce(vcat, cls))
-
-    return msms, edf, climout, threshout
-end
-
 
 function testarrays(sst, sstdate)
     errmsgdims = "Dimensions of temperature array and time do not match: "
