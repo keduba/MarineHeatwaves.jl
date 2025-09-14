29fd4ea3beb00c56ac00e5d5cb84a636419c6976
Kene Uba <jfk.uba@protonmail.ch>
2025-09-14 03:19:26 +0200

    added structs

--- src/fullmhw.jl
+++ src/fullmhw.jl
@@ -5,3 +5,69 @@ using Dates
 using NCDatasets
 using SparseArrays
 
+struct MHWrapper{T} 
+    anom::Array{T}
+    threshanom::Array{T}
+    onset::T
+    decline::T
+end
+
+struct MCWrapper{T}
+    anom::Array{T}
+    threshanom::Array{T}
+    onset::T
+    decline::T
+end
+
+struct EventsHW{T}
+    means::Array{T, 1}
+    minimaxes::Array{T, 1}
+    onset::Array{T, 1}
+    decline::Array{T, 1}
+    duration::Array{T, 1}
+    sums::Array{T, 1}
+end
+
+struct EventsCS{T}
+    means::Array{T, 1}
+    minimaxes::Array{T, 1}
+    onset::Array{T, 1}
+    decline::Array{T, 1}
+    duration::Array{T, 1}
+    sums::Array{T, 1}
+end
+
+struct MCS{T<:AbstractVecOrMat{<:AbstractFloat}}
+    temp::T
+    clim::T
+    thresh::T
+    exceeds::Array{Bool}
+end
+
+function MHW(temp, clim, thresh)
+    exc = _excess(temp, thresh)
+    MCS{typeof(temp), typeof(clim), typeof(thresh), typeof(exc)}(temp, clim, thresh, exc)
+end
+
+function MCS(temp, clim, thresh)
+    exc = _excess(thresh, temp)
+    MCS{typeof(temp), typeof(clim), typeof(thresh), typeof(exc)}(temp, clim, thresh, exc)
+end
+
+function MCS(temp, clim, thresh, event=:mhw)
+    exc = event == :mhw ? _excess(temp, thresh) : _excess(thresh, temp)
+    MCS{typeof(temp), typeof(clim), typeof(thresh), typeof(exc)}(temp, clim, thresh, exc)
+end
+
+struct MHWCSO{T<:AbstractVecOrMat{<:AbstractFloat}}
+    outtemp::T
+    outclim::T
+    outhresh::T
+    outexceeds::Array{Bool}
+    outmeans::T
+    outannuals::T
+    outpvalues::T
+    outcoeff::T
+    outrsquared::T
+end
+
